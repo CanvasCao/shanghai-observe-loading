78a9918cf53459d62449beda7a9df546a0cf1037
Cao Yuhao <1174478646@qq.com>
2017-08-24 00:23:05 +0800

    适配屏宽

--- src/response.js
+++ src/response.js
@@ -4,12 +4,9 @@ function respone() {
     var html = document.getElementsByTagName('html')[0];
 
     html.setAttribute('data-dpr', 2)
-    html.style['font-size'] = window.outerWidth / standardWidth * standardFontSize + 'px';
+    html.style['font-size'] = document.documentElement.clientWidth / standardWidth * standardFontSize + 'px';
     html.style['width'] = '100%';
     html.style['height'] = '100%';
-
     //overflow: 'hidden'
-
-
 }
 export default respone
