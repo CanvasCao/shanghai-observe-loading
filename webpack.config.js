35f05935b1d421d859a0a9de6125afa4930d61c1
Cao Yuhao <1174478646@qq.com>
2017-08-24 00:23:15 +0800

    WebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -13,14 +16,45 @@ module.exports = {
         port: 3003,
     },
     module: {
-        rules: [{
-            test: /\.css$/,
-            loader: "style-loader!css-loader"
-        }]
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
+            // {
+            //     test: /\.(png|jpg|gif)$/,
+            //     use: [
+            //         {
+            //             loader: 'url-loader',
+            //             options: {
+            //                 limit: 8192
+            //             }
+            //         }
+            //     ]
+            // }
+
+
+        ]
+
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
-        })
+        }),
+        new UglifyJSPlugin(),
+        new ExtractTextPlugin("./index.css"),//合并以后输出的css...
     ]
 };
